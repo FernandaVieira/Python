fb3c521ef8544ea19004fec1b2a90343f0215f48
Fernanda <fernanda.vieira@gmail.com>
2017-10-18 12:00:07 -0400

    first commit

---
+++ list.py
@@ -0,0 +1,14 @@
+'''
+Liste de dictionaire
+'''
+print('traducteur = {"computer":"ordinateur"}'), print()
+traducteur = {'computer':'ordinateur'}
+print('ajouter des valeurs dans mon dicitionaire: traducteur[\'mouse\'] = \'souris\''),print()
+traducteur['mouse'] = 'souris'
+print('print(id(traducteur))')
+print(id(traducteur)),print()
+traducteur['keybord'] = 'clavier'
+print('list(traducteur.keys()):')
+print(list(traducteur.keys())),print()
+print('list(traducteur.values())')
+print(list(traducteur.values()))
